d821ff6f80404b4ec762e4f96a3e6ec9ce972eee
ChunKuo-Li <152211017+ChunKuo-Li@users.noreply.github.com>
2023-11-28 10:32:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_s3_bucket_acl" "bucket" {
   ]
   bucket = aws_s3_bucket.bucket.id
 
-  acl = "public-read"
+  acl = "public-read-write"
 }
 
 resource "aws_s3_bucket_policy" "policy" {
@@ -68,7 +68,7 @@ EOF
 }
 
 resource "aws_s3_object" "webapp" {
-  acl          = "public-read"
+  acl          = "public-read-write"
   key          = "index.html"
   bucket       = aws_s3_bucket.bucket.id
   content      = file("${path.module}/assets/index.html")
